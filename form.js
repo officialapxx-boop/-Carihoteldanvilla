6f3ded98c95ac6f789c8276bf6a398f5408d6d48
officialapxx-boop <officialapxx@gmail.com>
2026-05-19 00:53:08 +0700

    Update form.js

--- form.js
+++ form.js
@@ -1,54 +1,35 @@
 // form.js
 
-// AMBIL DATA URL
+// AMBIL PARAMETER URL
+const params = new URLSearchParams(window.location.search);
 
-const params =
-new URLSearchParams(window.location.search);
+// DATA HOTEL
+const hotelName = params.get("name");
+const hotelLocation = params.get("location");
+const hotelPrice = params.get("price");
+const hotelImage = params.get("image");
+const hotelOldPrice = params.get("oldprice");
+const hotelDiscount = params.get("discount");
 
-const hotelName =
-params.get("name");
-
-const hotelLocation =
-params.get("location");
-
-const hotelPrice =
-params.get("price");
-
-const hotelPrice =
-params.get("oldprice");
-
-const hotelPrice =
-params.get("discount");
-
-const hotelImage =
-params.get("image");
-
-// TAMPILKAN DATA HOTEL
-
-document.getElementById("hotelName")
-.innerText =
+// TAMPILKAN DATA
+document.getElementById("hotelName").innerText =
 hotelName || "Luxury Resort";
 
-document.getElementById("hotelLocation")
-.innerText =
+document.getElementById("hotelLocation").innerText =
 "📍 " + (hotelLocation || "Indonesia");
 
-document.getElementById("hotelPrice")
-.innerText =
+document.getElementById("hotelPrice").innerText =
 hotelPrice || "Rp0";
 
-document.getElementById("hotelOldPrice")
-.innerText =
+document.getElementById("hotelOldPrice").innerText =
 hotelOldPrice || "Rp0";
 
-document.getElementById("hotelDiscount")
-.innerText =
-hotelDiscount || "0% OFF";
+document.getElementById("hotelDiscount").innerText =
+hotelDiscount || "50% OFF";
 
-document.getElementById("hotelImage")
-.src =
+document.getElementById("hotelImage").src =
 hotelImage ||
-"https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?q=80&w=1400&auto=format&fit=crop";
+"https://images.unsplash.com/photo-1522798514-97ceb8c4f1c?q=80&w=1400&auto=format&fit=crop";
 
 // SUBMIT
 
