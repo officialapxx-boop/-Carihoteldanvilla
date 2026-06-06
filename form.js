17a4cd8c6e5e8388974936433515953d53455a86
officialapxx-boop <officialapxx@gmail.com>
2026-06-06 14:53:37 +0700

    Update form.js

--- form.js
+++ form.js
@@ -1,19 +1,19 @@
-const params = new URLSearchParams(window.location.search);
+const p = new URLSearchParams(window.location.search);
 
 document.getElementById("hotelName").innerText =
-params.get("name") || "Luxury Villa";
+p.get("name") || "Luxury Villa";
 
 document.getElementById("hotelLocation").innerText =
-"📍 " + (params.get("location") || "Indonesia");
+"📍 " + (p.get("location") || "Indonesia");
 
 document.getElementById("hotelPrice").innerText =
-params.get("price") || "Rp2.500.000";
+p.get("price") || "Rp2.500.000";
 
 document.getElementById("hotelDiscount").innerText =
-params.get("discount") || "50% OFF";
+p.get("discount") || "50% OFF";
 
 document.getElementById("hotelImage").src =
-params.get("image") ||
+p.get("image") ||
 "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c?q=80&w=1400";
 
 /* SUBMIT */
